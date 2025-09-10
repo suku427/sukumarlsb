f43f8d05df29b6a75d92789445130bbf40e90439
unknown <kausthubh.peddibhotla@gmail.com>
2025-09-10 15:04:21 +0530

    created a new file

---
+++ big_num.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a ="))
+b = int(input("Enter b ="))
+c = int(input("Enter c ="))
+if (a>b and a>c):
+    print("Biggest number is ",a)
+elif (b>c):
+    print("Biggest number is ",b)
+else:
+    print("Biggest number is ",c)
